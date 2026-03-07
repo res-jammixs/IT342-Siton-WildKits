3274d9832748c0acfb28010b7bc210204a2129bd
LAB13_USER ON WS18 <L13Y09W18@LabHCI.net>
2026-03-07 09:19:01 +0800

    setup: initialization of folder structure

---
+++ web/backend/wildkits/src/main/java/edu/cit/siton/wildkits/WildkitsApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.siton.wildkits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildkitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildkitsApplication.class, args);
+	}
+
+}
